e97252b7aa066f8cf0dbbb8227e159e4c2f42fd8
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-10-16 18:25:14 +0300

    components finish

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const ApprovalCard = () => {
-
+const ApprovalCard = (props) => {
+  const {children} = props;
     return (
-        
-    )
-}
+        <div className='ui card'>
+            <div className='content'>{children}</div>
+            <div className='extra content'>
+                <div className='ui two buttons'>
+                    <div className='ui basic green button'>Approve</div>
+                    <div className='ui basic red button'>Decline</div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ApprovalCard;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className='ui container comments'>
-            <CommentDetail
-                avatar={Faker.image.avatar()}
-                author='Sam'
-                date='Today at 6:00'
-                text='Nice blog post!'
-            />
-             <CommentDetail
-                avatar={Faker.image.avatar()}
-                author='Jon'
-                date='Today at 7:00'
-                text='Nice blog post 2!'
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={Faker.image.avatar()}
+                    author='Sam'
+                    date='Today at 6:00'
+                    text='Nice blog post!'
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={Faker.image.avatar()}
+                    author='Jon'
+                    date='Today at 7:00'
+                    text='Nice blog post 2!'
+                />
+            </ApprovalCard>
         </div>
     );
 };
+
 ReactDOM.render(<App />, document.querySelector('#root'));
